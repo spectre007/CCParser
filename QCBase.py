28161533850ab88a34b75503c10cd2e6707a5a52
Alexander Zech <alexander.zech@unige.ch>
2017-10-12 16:29:13 +0200

    new printer function and variables

--- QCBase.py
+++ QCBase.py
@@ -18,15 +18,23 @@ class VarNames(object):
     exc_energy_abs = "exc_energy_abs"
     exc_energy_rel = "exc_energy_rel"
     mo_energies = "mo_energies"
+    nuc_repulsion = "nuc_rep"
     
-    # -- Transition properties --
+    # -- Excited States --
     osc_str = "osc_str"
     amplitudes = "ampl"
+    diff_dens_anl = "diff_dens_anl"
+    trans_dens_anl = "trans_dens_anl"
     
-    # -- Misc --
+    # -- General --
+    xyz_coordinates = "xyz"
     pt_order = "pt_order"
+    overlap_matrix = "S"
+    orthonorm_matrix = "X"
+    mo_coefficients = "C"
+    n_bas = "nbas"
     
-    # -- Technical --
+    # -- Issues & Warnings --
     has_converged = "state_cnvgd"
     
     # -- Frozen-Density Embedding
@@ -34,6 +42,8 @@ class VarNames(object):
     fde_trust = "fde_trust"
     fde_delta_lin = "fde_delta_lin"
     fde_electrostatic = "fde_elstat_int"
+    fde_timing = "fde_times"
+    fde_scf_vemb = "fde_scf_vemb"
     
     
     
@@ -53,6 +63,18 @@ class QCMethod(object):
     
     def add_variable(self, func_name, var_name):
         self.map[func_name] = var_name
+        
+    def print_parsed(self, var_name, description):
+        """Print short parsing information
+        
+        Parameters
+        ----------
+        var_name : string
+            Variable name from the VarNames class
+        description : string
+            Short (!) description of the object that was parsed.
+        """
+        print("[results.{0:}] Parsed {1:}.".format(var_name, description))
         
 #    def generate_map(self, func_name, var_name):
 #        self.var[var_name] = func_name
