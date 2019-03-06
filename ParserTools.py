76b0d2675263663563442e1ce697f803cd69dc72
spectre007 <alexander.zech@unige.ch>
2019-03-06 17:18:03 +0100

    added is_square function

--- ParserTools.py
+++ ParserTools.py
@@ -6,12 +6,20 @@ Created on Wed Sep 19 14:43:23 2018
 @author: alex
 """
 
+import math
 
 
-def isfloat(string):
+def is_float(string):
     """Check if a string can be converted to float."""
     try:
         float(string)
         return True
     except ValueError:
+        return False
+
+def is_square(integer):
+    root = math.sqrt(integer)
+    if int(root + 0.5) ** 2 == integer: 
+        return True
+    else:
         return False
